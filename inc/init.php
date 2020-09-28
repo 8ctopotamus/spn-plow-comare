eec4ed783343463890f50f0cfb966cc8c805d205
Josh <zylo.codes@gmail.com>
2020-09-27 21:16:50 -0500

    Fix production bundle loading in init

--- inc/init.php
+++ inc/init.php
@@ -14,7 +14,7 @@ function spn_plow_compare_load_shortcode_resources() {
 	
   wp_register_style( 'animate_css', '//cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css' );
 
-	if ( $isLocalhost && !$testing ) {
+	if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
     $react_app_js = 'http://localhost:3000/static/js/bundle.js';
   } else {
 		// production
